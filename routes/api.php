71f189003d0fdeba6d3736bc311961035d5416e0
Arya Dwi Putra <aryadptr.developer@gmail.com>
2025-06-06 20:43:14 +0700

    feat: Define API routes for user login and registration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('api')->group(function () {
+    Route::post('/login', App\Http\Controllers\Api\V1\LoginController::class)
+        ->name('api.login');
+    Route::post('/register', App\Http\Controllers\Api\V1\RegisterController::class)
+        ->name('api.register');
+});
